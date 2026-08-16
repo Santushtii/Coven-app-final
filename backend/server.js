4da3e7f4da5592c8163c1240271c85969d83955c
Santushti Lamichhane <santushti22@gmail.com>
2026-08-16 22:53:25 +0545

    summer class 10th commit

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
+      "http://localhost:5174",
       "https://coven-app-final-frontend.onrender.com",
     ],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
